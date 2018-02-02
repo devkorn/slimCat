476517c53ca702b77bab69b10d8fc0327993fc0b
Piotr Kornecki <pikornecki@gmail.com>
2018-02-02 09:05:28 +0100

    slimcat_00001

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang=”pl”>
+    
+<head>
+    <title>SlimCat v1.1 </title>
+     
+<?php
+    include_once("controllers.php");
+    $controller = new Controllers();
+    $controller->setBlock('head');
+?>
+    <!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->   
+
+</head>
+    
+<body class="loginPage">
+    <div id="cat-content">
+        <header>
+        </header>
+        <main>
+            <?php
+                $controller->setBlock('main');
+            ?>
+        </main>
+        <div id="cat-clearfix"></div>
+    </div>
+    <footer role=”contentinfo” id="cat-footer">
+        <div id="cat-copyrightWrap">
+            <div class="cat-container">
+                <p>Wszelkie prawa zastrzeżone. Kopiowanie bez zgody autora zabronione.</p>
+            </div>
+        </div>
+    </footer>
+    <!—Skrypty Javascript -->
+    <!-- <script src=”javascript.js”> </script> -->
+</body>
+</html>
